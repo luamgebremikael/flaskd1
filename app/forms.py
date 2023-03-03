174f2c5411ec9be9bd9833e3f4f69ecfa5c64b19
luamgebremikael <123391464+luamgebremikael@users.noreply.github.com>
2023-03-03 00:42:34 -0500

    first commit

--- app/forms.py
+++ app/forms.py
@@ -2,9 +2,24 @@ from flask_wtf import FlaskForm
 from wtforms import StringField,PasswordField,SubmitField
 from wtforms.validators import DataRequired,EqualTo
 
-class RegisterForms(FlaskForm):
+class signupForms(FlaskForm):
     username= StringField('Username',validators=[DataRequired()])
     email= StringField('email',validators=[DataRequired()])
     password= PasswordField('Password',validators=[DataRequired()])
+    Firstname=StringField('Firstname',validators=[DataRequired()])
+    Lastname=StringField('Lastname',validators=[DataRequired()])
     confirm_password= PasswordField('Confirm Password',validators=[DataRequired(),EqualTo('password')])
-    submit=SubmitField('Register')
+    submit=SubmitField('signup')
+
+class LoginForms(FlaskForm):
+    email= StringField('email',validators=[DataRequired()]) 
+    password= PasswordField('Password',validators=[DataRequired()])  
+    submit=SubmitField('Login')
+
+class carForms(FlaskForm):
+    make= StringField('make',validators=[DataRequired()])
+    model= StringField('model',validators=[DataRequired()])
+    year= PasswordField('year',validators=[DataRequired()])
+    color=StringField('color',validators=[DataRequired()])
+    price= PasswordField('price',validators=[DataRequired(),EqualTo('password')])
+    submit=SubmitField('signup')
--- app/routes.py
+++ app/routes.py
@@ -1,31 +1,32 @@
-from flask import render_template
+from flask import render_template,flash,redirect
 from app import app
-from app.forms import RegisterForms
+from app.forms import signupForms,LoginForms,carForms
 
 
 
 @app.route('/')
 def rest():
-    resturants={
-        'employers':('jerry','Abiel'),
-        'Tabels':['Tabel 1','Tabel 2','Tabel 3','Tabel 4','Tabel 5']
-
-    }
-    return render_template('rest.html.jinja', cafe=resturants, title='Home')
+    carlist={
+        'carlist':('Trucks','SUVs'),
+        'Brands':['Honda','Nissan','Ford','Jeep','volvo']}
+    return render_template('rest.html.jinja', car=carlist, title='Home')
 
 @app.route('/Home')
 def Home():
     return render_template('Home.jinja') 
-@app.route('/Login')   
+@app.route('/Login',methods=['GET','POST'])   
 def Login():
-    return render_template('Login.jinja')  
-@app.route('/Register')  
-def Register():
-    form=RegisterForms()
-    return render_template('Register.jinja',form=form)
-@app.route('/About')    
-def About():
-    return render_template('About.jinja') 
-@app.route('/Blog')   
-def Blog():
-    return render_template('Blog.jinja')    
+    form=LoginForms()
+    return render_template('Login.jinja',form=form)  
+@app.route('/signup',methods=['GET','POST'])  
+def signup():
+    form=signupForms()
+    if form.validate_on_submit:
+        flash(f'Request to signup{form.username}sucessfull!')
+        
+    return render_template('signup.jinja',form=form)
+@app.route('/car')    
+def car():
+    form=carForms()
+    return render_template('car.jinja',form=form)
+ 
